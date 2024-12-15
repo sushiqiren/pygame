ab6f146f57f6e4cea6e93c5bfe8a0309e321419b
sushiqiren <hzfulideman@gmail.com>
2024-12-15 21:42:48 +1100

    update program to add player and asteroid

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,12 +14,48 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    object_toUse = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for updateObject in updatable:
+            updateObject.update(dt)
+
+        for obj in asteroids:
+            if obj.collide(player):
+                print("Game over!")
+                sys.exit()
+
+        for drawObject in drawable:
+            drawObject.draw(screen)
+
+        
+        # player.draw(screen)
         pygame.display.flip()
+        dt = object_toUse.tick(60) / 1000
+        
+
+    
+    
     
 
 if __name__ == "__main__":
